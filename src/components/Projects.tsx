c7209d9802fd2a9549a7ea519d4b7a9fb1d5da94
hub-ry <rhubbart@purdue.edu>
2026-01-26 22:19:28 -0500

    portfolio page no content

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,15 +4,17 @@ import { ArrowCycle } from "./ArrowCycle";
 
 export function Projects() {
   return (
-    <section className="px-6 py-12">
-      <h2 className="text-2xl font-bold mb-6 text-white">
-        Projects
-        <ArrowCycle />
-      </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8">
-        {projects.map((p, i) => (
-          <ProjectCard key={i} project={p} />
-        ))}
+    <section className="px-6 py-12 fade-in">
+      <div className="max-w-4xl mx-auto ">
+        <h2 className="text-2xl font-bold mb-24 text-white">
+          Projects
+          <ArrowCycle />
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8">
+          {projects.map((p, i) => (
+            <ProjectCard key={i} project={p} />
+          ))}
+        </div>
       </div>
     </section>
   );
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,33 +1,48 @@
+import sshProject from "../assets/ssh-project.png"
+import lance from "../assets/lance.jpg";
+import gymProject from "../assets/gym-proj.png"
+
 export type Project = {
   title: string
   subtitle: string
   image: string
   type: "Project" | "Presentation"
+  start: string
+  end: string
 }
+
 
 export const projects: Project[] = [
   {
-    title: "TICYL",
-    subtitle: "Topological Interlocking Cylinder Generator",
-    image: "/images/ticyl.png",
+    title: "Full Stack Ticketing System",
+    subtitle: "Built for Purdue GrandPrix",
+    image: lance,
     type: "Project",
+    start: 'string',
+    end: 'sting'
   },
   {
-    title: "A Glimpse Of Configurations",
-    subtitle: "Topological Interlocking Presentation",
-    image: "/images/configs.png",
+    title: "Attendace Automation",
+    subtitle: "Built for Purdue GrandPrix",
+    image: lance,
     type: "Presentation",
+    start: 'string',
+    end: 'sting'
   },
   {
-    title: "Assemblable Polyominoes",
-    subtitle: "Interlocking Presentation",
-    image: "/images/polyominoes1.png",
+    title: "Building Personal Server",
+    subtitle: "",
+    image: sshProject,
     type: "Presentation",
+    start: 'Dec 2025',
+    end: 'Jan 2026'
   },
   {
-    title: "Interlocking Polyominoes",
-    subtitle: "Colorful Grid Project",
-    image: "/images/polyominoes2.png",
+    title: "Protocol Tracker",
+    subtitle: "Health Database built for personal use",
+    image: gymProject,
     type: "Project",
+    start: 'Aug 2025',
+    end: 'Present'
   },
 ]
